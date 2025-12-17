eb950c5a0a087901d12bdcdef0a1674e36ef68c6
falahal23 <falahal24si@mahasiswa.pcr.ac.id>
2025-12-17 09:22:11 +0700

    perubahan14

--- database/seeders/CreateFirstUserSeeder.php
+++ database/seeders/CreateFirstUserSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\User;
@@ -15,12 +14,14 @@ class CreateFirstUserSeeder extends Seeder
         $faker = Faker::create('id_ID');
 
         // 🔹 1. Buat admin default (Super Admin)
-        User::create([
-            'name'     => 'Falahal Nabil',
-            'email'    => 'falahal@gmail.com',
-            'password' => Hash::make('123456781'),
-            'role'     => 'Super Admin',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'falahal@gmail.com'],
+            [
+                'name'     => 'Falahal Nabil',
+                'password' => Hash::make('2401010'),
+                'role'     => 'Super Admin',
+            ]
+        );
 
         // 🔹 2. Buat 100 user random
         for ($i = 0; $i < 100; $i++) {
